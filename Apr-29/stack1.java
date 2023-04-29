a9e1739315f96db6792c17c0403c9b4a277d2c9d
Harika <lakshmimaha683@gmail.com>
2023-04-29 10:55:06 +0530

    Added files

---
+++ Apr-29/stack1.java
@@ -0,0 +1,188 @@
+class Stack
+{
+  static int MAX = 100;
+  int top;
+  int a[] = new int[MAX];	
+
+
+  boolean isEmpty ()
+  {
+    return (top < 0);
+  }
+  Stack ()
+  {
+    top = -1;
+  }
+
+  boolean push (int x)
+  {
+    if (top >= (MAX - 1))
+      {
+	System.out.println ("Overflow condition reached");
+	return false;
+      }
+    else
+      {
+	a[++top] = x;
+	System.out.println (x + " pushed into stack");
+	return true;
+      }
+  }
+   int pop ()
+  {
+    if (top < 0)
+      {
+	System.out.println ("Underflow condition reached");
+	return 0;
+      }
+    else
+      {
+	int x = a[top--];
+	return x;
+      }
+  }
+
+
+  int peek ()
+  {
+    if (top < 0)
+      {
+	System.out.println ("Underflow condition");
+	return 0;
+      }
+    else
+      {
+	int x = a[top];
+	return x;
+      }
+  }
+}
+
+class Main
+{
+  public static void main (String args[])
+  {
+    Stack stk = new Stack ();
+      stk.push (20);
+      stk.push (40);
+      stk.push (60);
+      System.out.println ("element poped out : " + stk.pop ());
+  }
+}
+-------------------------------------------------------------------------
+import java.util.Scanner;
+class Heap
+{
+ 
+    private int[] heapArray;
+    private int maxSize; 
+    private int heapSize; 
+ 
+    public Heap(int mx) 
+    {
+        maxSize = mx;
+        heapSize = 0;
+        heapArray = new int[maxSize]; 
+    }
+    public boolean isEmpty() 
+    {
+        return heapSize == 0;
+    
+    public boolean insert(int ele) 
+    {
+        if (heapSize + 1 == maxSize)
+            return false;
+        heapArray[++heapSize] = ele;
+        int pos = heapSize;
+        while (pos != 1 && ele > heapArray[pos/2])
+        {
+            heapArray[pos] = heapArray[pos/2];
+            pos /=2;
+        }
+        heapArray[pos] = ele;    
+        return true;
+    } 
+    public int remove()
+    {
+        int parent, child;
+        int item, temp;
+        if (isEmpty() )
+            throw new RuntimeException("Error : Heap empty!");
+ 
+        item = heapArray[1];
+        temp = heapArray[heapSize--];
+ 
+        parent = 1;
+        child = 2;
+        while (child <= heapSize)
+        {
+            if (child < heapSize && heapArray[child] < heapArray[child + 1])
+                child++;
+            if (temp >= heapArray[child])
+                break;
+ 
+            heapArray[parent] = heapArray[child];
+            parent = child;
+            child *= 2;
+        }
+        heapArray[parent] = temp;
+ 
+        return item;
+    }
+    public void displayHeap()
+    {
+       System.out.print("\nHeap array: ");    
+        for(int i = 1; i <= heapSize; i++)
+            System.out.print(heapArray[i] +" ");
+        System.out.println("\n");
+    }  
+}
+public class HeapTest
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Heap Test\n\n");
+        System.out.println("Enter size of heap");
+        Heap h = new Heap(scan.nextInt() );
+ 
+        char ch;
+        do    
+        {
+            System.out.println("\nHeap Operations\n");
+            System.out.println("1. insert ");
+            System.out.println("2. delete item with max key ");
+            System.out.println("3. check empty");
+ 
+            boolean chk;       
+            int choice = scan.nextInt();
+            switch (choice)
+            {
+            case 1 : 
+                System.out.println("Enter integer element to insert");
+                chk = h.insert( scan.nextInt() ); 
+                if (chk)
+                    System.out.println("Insertion successful\n");
+                else
+                    System.out.println("Insertion failed\n");                    
+                break;                          
+            case 2 : 
+                System.out.println("Enter integer element to delete");
+                if (!h.isEmpty())
+                    h.remove();
+                else
+                    System.out.println("Error. Heap is empty\n");   
+                break;                         
+            case 3 : 
+                System.out.println("Empty status = "+ h.isEmpty());
+                break;         
+            default : 
+                System.out.println("Wrong Entry \n ");
+                break;       
+            }
+            h.displayHeap();
+            System.out.println("\nDo you want to continue (Type y or n) \n");
+            ch = scan.next().charAt(0);                        
+        } while (ch == 'Y'|| ch == 'y');  
+    }
+}
