7f9a34666674151abf92fc9c5c1aa0b19108bed5
Harika <lakshmimaha683@gmail.com>
2023-05-04 09:09:10 +0530

    training may 4

---
+++ May4/factorialTrailingzeros.java
@@ -0,0 +1,26 @@
+package May4;
+
+public class factorialTrailingzeros {
+
+	
+	static int findTrailingZeros(int n)
+	{
+		if (n < 0) 
+			return -1;
+
+		int count = 0;
+
+		for (int i = 5; n / i >= 1; i *= 5)
+			count += n / i;
+
+		return count;
+	}
+
+	public static void main(String[] args)
+	{
+		int n = 100;
+		System.out.println("Count of trailing 0s in " + n
+						+ "! is "
+						+ findTrailingZeros(n));
+	}
+}
